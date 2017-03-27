15d8adbad0232c6fa26461671ea5938c6076eeb2
rafaeel16 <rafaeel.16@gmail.com>
2017-03-26 21:58:18 -0300

    delete folder

--- Firebase/public/js/app.js
+++
@@ -1,9 +0,0 @@
-// Initialize Firebase
-var config = {
-    apiKey: "AIzaSyDNEEoIC83c8I39911FXxdN5lN9kDIddU4",
-    authDomain: "curso-85172.firebaseapp.com",
-    databaseURL: "https://curso-85172.firebaseio.com",
-    storageBucket: "curso-85172.appspot.com",
-    messagingSenderId: "185294215148"
-};
-firebase.initializeApp(config);
