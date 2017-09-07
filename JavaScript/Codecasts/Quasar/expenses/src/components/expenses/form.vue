9f18546312f38da09fcc594d0a4ef32d308beb55
rafaeel16 <rafaeel.16@gmail.com>
2017-09-07 00:18:01 -0300

    aula 4

---
+++ JavaScript/Codecasts/Quasar/expenses/src/components/expenses/form.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Esta string esta vindo do cc-form</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ JavaScript/Codecasts/Quasar/expenses/src/components/expenses/list.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Componente para a lista</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
